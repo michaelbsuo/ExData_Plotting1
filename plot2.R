6b9afc1c88d8476071c5a4c3d40914dbb83cb307
Michaelbsuo <michael.suobx@hotmail.com>
2015-03-08 13:36:30 -0400

    add annotations to R scripts

--- plot2.R
+++ plot2.R
@@ -1,10 +1,15 @@
+## Loading data
 dataset <- read.csv("./4. Exploratory Data Analysis/Week 1/Assignment/household_power_consumption.txt", sep = ";", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), na.strings = "?")
+
+## Subset dates 2007-02-01 and 2007-02-02
 subset <- dataset$Date == "1/2/2007" | dataset$Date == "2/2/2007"
 dataset_sub <- dataset[subset, ]
 
+## Convert the Date and Time
 date_time <- paste(dataset_sub$Date, dataset_sub$Time)
 dataset_sub$DateTime <- strptime(date_time, "%d/%m/%Y %H:%M:%S")
 
+## Making plots
 png("plot2.png", height = 480, width = 480)
 with(dataset_sub, plot(DateTime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
 dev.off()
